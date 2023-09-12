776a6e3b4f3869975b06c85fd78edd2d3147c733
Singtodev <138797225+Unkeloc4@users.noreply.github.com>
2023-09-13 01:40:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+    $title = "Pizza Project";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title?></title>
+</head>
+<body>
+</body>
+</html>
