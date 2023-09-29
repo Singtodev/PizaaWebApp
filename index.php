5e4423fa373a3fe41bc87651280db28363ad667d
Singtodev <138797225+Unkeloc4@users.noreply.github.com>
2023-09-30 05:35:06 +0700

    add filter pizza and add components

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php 
 
     include_once('./utils/condb.php');
+    include_once('./components/view/pizza_card_item.php');
 ?>
 
 <?php
@@ -16,6 +17,7 @@
     <title><?php echo $title?></title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body class="bg-gray-100">
     <div class="mx-auto lg:w-full lg:mx-0">
@@ -53,54 +55,156 @@
         </div>
     </div>
 
-    <div class="max-w-[80rem] lg:max-w-[110rem] mx-auto py-14">
+    <div class="max-w-[80rem] lg:max-w-[110rem] mx-auto lg:py-14 mb-8">
 
-        <div class="w-full h-[30rem] bg-gradient-to-r from-orange-500 mb-14 py-10 px-10">
+        <div class="w-full h-[30rem] bg-gradient-to-r from-orange-500 lg:mb-14 py-10 px-10">
                 <div class="text-5xl font-bold py-2 mb-5"> Buy to day</div>
                 <div class="text-5xl font-bold py-2 mb-3"> FREE *</div>
                 <div class="text-4xl font-bold py-2 mb-2"> Delivery</div>
                 <div class="text-2xl py-3">32 march - 45 Oct   2077 only</div>
                 <div class="text-md max-w-[25rem] py-3">This promotion is available in limited quantities, so hurry and buy quickly.  </div>
         </div>
-        <div class="grid grid-cols-4 gap-x-10 gap-y-10 cursor-pointer">
-            <?php
-            $sql = "SELECT * FROM food , food_type , food_size where food.f_type_id = food_type.f_type_id and food.f_size_id = food_size.f_size_id order by food.f_id";
-            $result = $condb->query($sql);
 
-            while($row = $result->fetch_assoc()) { ?>
+
+
+        <div class="flex flex-col md:flex-row gap-x-3 p-5">
+            <div class="w-full lg:w-[20%] bg-white px-5 inline-block lg:min-h-[40rem]">
+                <div class="checkbox_group flex flex-col gap-x-4 my-4">
+                    <p class="size">ขนาดไซด์</p>
+                    <?php
                     
-     
-                        <div class="relative bg-white border-4 border-white  rounded-md  hover:border-[#131921] transition-all duration-300">
-                            <div class="card w-full h-[24rem] cursor-pointer bg-gray-300 bg-cover object-cover bg-center " style="background-image: url('<?= $row['f_image'] ?>')">
-                            </div>
-                            <div class="content p-2">
-                                <div class="header">
-                                    <div class="title text-bold text-xl"><?= $row['f_name'] ?></div>
-                                </div>
-                                <div class="body pt-3">
-                                </div>
-                                <div class="footer py-3 flex flex-row justify-between">
-                                    <div class="flex flex-row gap-x-4">
-                                        <div><span class="text-red-700"><?= $row['f_price'] ?></span> THB </div>
-                                        <div class="size"> <span class="bg-[#131921]  px-4 text-white rounded-2xl">Size  <?= strtoupper($row['f_size_name'] ) ?></span></div>
-                                    </div>
-    
-                                    <div class="flex items-center gap-x-3"> 
-                                        <i class="fa-solid fa-plus inline-block text-green-500  text-2xl"></i>
-                                        <a href="show.php?f_id=<?php echo $row['f_id']?>">
-                                            <i class="fa-solid fa-search inline-block text-green-500  text-2xl"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                    $sql = "SELECT  * from food_size";
+                    $result = $condb->query($sql);
+                    while($row = $result->fetch_assoc()) { ?>
+                            <label class="flex gap-x-3">
+                                <input  
 
-                        </div>
-          
+                                    type="checkbox" 
+                                    data-name="<?php echo $row['name'] ?>"
+                                    class="cursor-pointer checkbox_trigger checkbox_size"> <?php echo $row['name'] ?></label>
+                    <?php } ?>
+                </div>
 
-            <?php } ?>
+                <div class="checkbox_group flex flex-col gap-x-4 my-4">
+                    <p class="size">ประเภท</p>
+                    <?php
+                    
+                    $sql = "SELECT  * from food_type";
+                    $result = $condb->query($sql);
+                    while($row = $result->fetch_assoc()) { ?>
+                            <label class="flex gap-x-3">
+                                <input  
+   
+                                    type="checkbox" 
+                                    data-name="<?php echo $row['name'] ?>"
+                                    class="cursor-pointer checkbox_trigger checkbox_type"> <?php echo $row['name'] ?></label>
+                    <?php } ?>
+                </div>
+
+                <div class="checkbox_group flex flex-col gap-x-4 my-4">
+                    <p class="size">ขอบ</p>
+                    <?php
+                    
+                    $sql = "SELECT  * from food_crust";
+                    $result = $condb->query($sql);
+                    while($row = $result->fetch_assoc()) { ?>
+                            <label class="flex gap-x-3">
+                                <input  
+
+                                    type="checkbox" 
+                                    data-name="<?php echo $row['name'] ?>"
+                                    class="cursor-pointer checkbox_trigger checkbox_crust"> <?php echo $row['name'] ?></label>
+                    <?php } ?>
+                </div>
+
+
+
+            </div>
+            <div class="w-full lg:w-[80%]">
+                <section id="section-pizza-show">
+                    <div class="w-full grid grid-cols-1 md:grid-cols-2 md:gap-x-5 lg:grid-cols-4  lg:gap-x-10 gap-y-10">
+                        <?php
+                        $sql = "SELECT      food.fid, food.description , food.price ,food.image, 
+                                            food.name as f_name,
+                                            food_type.name as f_type_name,
+                                            food_size.name as f_size_name,
+                                            food_crust.name as f_crust_name
+                                FROM        food , food_type , food_size, food_crust
+                                where       food.ftid = food_type.ftid 
+                                and         food.fsid = food_size.fsid
+                                and         food.fcid = food_crust.fcid
+                                order by food.fid";
+
+
+                        $result = $condb->query($sql);
+                        while($row = $result->fetch_assoc()) { ?>
+
+                                <?php
+                                    $card = new PizzaCardItem();
+                                    $card->build($row);
+                                ?>
+
+                        <?php } ?>
+                    </div>
+                </section>
+            </div>
         </div>
+
+
+
+
+
     </div>
 
+
+
+    <script>
+
+
+            function getAllValueInCheckBoxSize(hook){
+                var x = document
+                .querySelectorAll(hook);
+
+                var active = [];
+                var all_keys = [];
+                for(let i = 0; i < x.length; i++){
+                    if(x[i].checked){
+                        active.push(x[i].attributes['data-name'].value)
+                    }
+
+                    all_keys.push(x[i].attributes['data-name'].value)
+                }
+                return {active , all_keys};
+            }
+
+
+            $(document).ready(function(){
+                $(".checkbox_trigger").click(function(){
+
+                    var QuerySizes = getAllValueInCheckBoxSize(".checkbox_size");
+                    var QueryTypes = getAllValueInCheckBoxSize(".checkbox_type");
+                    var QueryCrusts = getAllValueInCheckBoxSize(".checkbox_crust");
+
+                    $.ajax({
+                        url: './query/pizza_data.php',
+                        method: 'GET',
+                        data: { 
+                            sizes : QuerySizes.active.length > 0 ? QuerySizes.active : QuerySizes.all_keys,
+                            types:  QueryTypes.active.length > 0 ? QueryTypes.active : QueryTypes.all_keys,
+                            crusts:  QueryCrusts.active.length > 0 ? QueryCrusts.active : QueryCrusts.all_keys
+                        },
+                        success: function(response) {   
+                            $('#section-pizza-show').html(response);
+                        },
+                        error: function() {
+                            console.log("filter error");
+                        }
+                    });
+
+                });
+            });
+
+    </script>
 
 
 </body>
